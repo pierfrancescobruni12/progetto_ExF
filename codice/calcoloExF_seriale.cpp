2a545839240a20ecd3fcbf944dc642e131b552ae
pierfrancescobruni12 <60651958+pierfrancescobruni12@users.noreply.github.com>
2021-12-01 14:46:16 +0100

    Update calcoloExF_seriale.cpp

--- codice/calcoloExF_seriale.cpp
+++ codice/calcoloExF_seriale.cpp
@@ -321,7 +321,7 @@ int main(int argc, char* argv[]) {
 
 	auto end = chrono::steady_clock::now();
 	auto diff = end - start;
-	cout << chrono::duration <double, milli>(diff).count() << " ms" << endl;
+	// cout << chrono::duration <double, milli>(diff).count() << " ms" << endl;
 
 
 	return 0;
